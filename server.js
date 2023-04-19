465983bc387c0c0cc93e3b1de1ff1ac0a79304b0
Chris McNamara <cmcnamara1991@icloud.com>
2023-04-19 16:20:28 -0500

    Start post request on body

--- server.js
+++ server.js
@@ -10,12 +10,27 @@ app.get('/notes', (req,res)=> {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
-// app.get('/assets/css/styles.css', (req, res)=> {
-//     res.sendFile(path.join(__dirname, './public/assets/css/styles.css'))
-// })
+app.get('/api/notes', (req,res)=> {
+    res.sendFile(path.join(__dirname, './db/db.json'))
+})
+
+app.post('/api/notes', (req, res)=> {
+    console.log(req.body)
+})
+
+app.get('/*', (req,res)=> {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(3001, () => {
     console.log('server is running')
 });
 
- 
+
+
+
+
+
+// app.get('/assets/css/styles.css', (req, res)=> {
+//     res.sendFile(path.join(__dirname, './public/assets/css/styles.css'))
+// })

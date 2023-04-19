11c55006ab2383d6f06728fd029331f86036a703
Chris McNamara <cmcnamara1991@icloud.com>
2023-04-19 14:33:07 -0500

    Add pathing route for CSS

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/notes', (req,res)=> {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+app.get('/assets/css/style.css', (req, res)=> {
+    res.senfFile(path.join(__dirname, './public/assets/css/style.css'));
+});  
+
+
 app.get('/goodbye', (req, res)=> {
     res.send('see you later!')
 });

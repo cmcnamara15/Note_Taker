511466a27f2dd482221c80339eee2c46b185a443
Chris McNamara <cmcnamara1991@icloud.com>
2023-04-19 15:47:59 -0500

    Replace css pathing with express.static middleware.

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const path = require('path');
 
 const app = express();
 
+// MIDDLEWARE
+app.use(express.static('./public'))
+
 app.get('/notes', (req,res)=> {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
 
-app.get('/assets/css/styles.css', (req, res)=> {
-    res.sendFile(path.join(__dirname, './public/assets/css/styles.css'))
-})
+// app.get('/assets/css/styles.css', (req, res)=> {
+//     res.sendFile(path.join(__dirname, './public/assets/css/styles.css'))
+// })
 
 app.listen(3001, () => {
     console.log('server is running')
 });
 
+ 

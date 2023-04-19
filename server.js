408e5600988dbccd93fded9f695456a1c9af73c6
Chris McNamara <cmcnamara1991@icloud.com>
2023-04-19 14:26:19 -0500

    Fix pathing for notes.html

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.get('/notes', (req,res)=> {
-    res.sendFile('./public/notes.html')
-}) 
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.get('/goodbye', (req, res)=> {
     res.send('see you later!')
-})
+});
 
 app.listen(3001, () => {
     console.log('server is running')

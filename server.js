9b7d41f2270e163f65d79ea1cc118e537a238c90
Chris McNamara <cmcnamara1991@icloud.com>
2023-04-19 15:26:34 -0500

    Fix typo in css pathing

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 app.get('/notes', (req,res)=> {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
-app.get('/assets/css/style.css', (req, res)=> {
-    res.senfFile(path.join(__dirname, './public/assets/css/style.css'));
-});  
-
-
-app.get('/goodbye', (req, res)=> {
-    res.send('see you later!')
-});
+app.get('/assets/css/styles.css', (req, res)=> {
+    res.sendFile(path.join(__dirname, './public/assets/css/styles.css'))
+})
 
 app.listen(3001, () => {
     console.log('server is running')
 });
-
-
 

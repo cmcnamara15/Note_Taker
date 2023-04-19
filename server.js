a765baa7f8af90e90a1da85da1a21f3bfe45b1af
Chris McNamara <cmcnamara1991@icloud.com>
2023-04-19 14:22:44 -0500

    Add get route to notes.html

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+
+app.get('/notes', (req,res)=> {
+    res.sendFile('./public/notes.html')
+}) 
+
+app.get('/goodbye', (req, res)=> {
+    res.send('see you later!')
+})
+
 app.listen(3001, () => {
     console.log('server is running')
 });
